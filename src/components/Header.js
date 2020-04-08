f4d9353fa321a55672f8fe95ab3004ba8201ea5e
Mark Jenquine <mjenquine@gmail.com>
2020-04-08 13:21:13 -0500

    started header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
+class Header extends Component {
+  render() {
+    return (
+      
+    )
+  }
+}
